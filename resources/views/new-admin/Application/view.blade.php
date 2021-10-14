9ed09d7913b5abf38d2da1c86cc8edc815414558
razin223 <razin223@gmail.com>
2021-10-15 00:28:19 +0600

    Update view.blade.php

--- resources/views/new-admin/Application/view.blade.php
+++ resources/views/new-admin/Application/view.blade.php
@@ -3,7 +3,29 @@
 @section("content")
 <div class="row">
     <div class="col-12">
+        <?php
+        $Data = \App\Application::find($id);
+        $User = $Data->getUser;
+        $Sector = $Data->getSector;
 
+        if ($Data != null) {
+            ?>
+            <h4 class="text-center">{{$Sector->sector_name}}</h4>
+            <table class="table">
+                <tbody>
+                    <tr>
+                        <td>Name: </td>
+                        <td>{{$User->name}}</td>
+                    </tr>
+                </tbody>
+            </table>
+            <?php
+        } else {
+            ?>
+            <h4 class="text-center text-danger">No data found.</h4>
+            <?php
+        }
+        ?>
     </div>
 
 </div>
