0ceeab1dd5085828f94d9ee47b2c369eb2dec290
razin223 <razin223@gmail.com>
2021-10-15 00:36:43 +0600

    Update view.blade.php

--- resources/views/new-admin/Application/view.blade.php
+++ resources/views/new-admin/Application/view.blade.php
@@ -11,11 +11,61 @@
         if ($Data != null) {
             ?>
             <h4 class="text-center">{{$Sector->sector_name}}</h4>
-            <table class="table">
+            <table class="table table-bordered">
                 <tbody>
                     <tr>
                         <td style="width: 150px">Name: </td>
                         <td>{{$User->name}}</td>
+                    </tr>
+                    <tr>
+                        <td>Date of Birth: </td>
+                        <td>{{date("d-m-Y",strtotime($User->date_of_birth))}}</td>
+                        <td style="width: 150px">Gender: </td>
+                        <td>{{$User->gender}}</td>
+                    </tr>
+                    <tr>
+                        <td>Email: </td>
+                        <td>{{$User->email}}</td>
+                    </tr>
+                    <tr>
+                        <td>Father: </td>
+                        <td>{{$User->father_name}}</td>
+                    </tr>
+                    <tr>
+                        <td>Mother: </td>
+                        <td>{{$User->mother_name}}</td>
+                    </tr>
+                    <tr>
+                        <td>Guardian: </td>
+                        <td>{{$User->guardian_name}}</td>
+                    </tr>
+                    <tr>
+                        <td>Guardian Mobile: </td>
+                        <td>{{$User->guardian_mobile_no}}</td>
+                    </tr>
+                    <tr>
+                        <td>Guardian Email: </td>
+                        <td>{{$User->guardian_email}}</td>
+                    </tr>
+                    <tr>
+                        <td>Present Address: </td>
+                        <td>{{$User->address}}, District: {{$User->getDistrict->name}}, Division: {{$User->getDistrict->getDivision->name}}</td>
+                    </tr>
+                    <tr>
+                        <td>Permanent Address: </td>
+                        <td>{{$User->permanent_address}}</td>
+                    </tr>
+                    <tr>
+                        <td>Guardian Email: </td>
+                        <td>{{$User->guardian_email}}</td>
+                    </tr>
+                    <tr>
+                        <td>Guardian Email: </td>
+                        <td>{{$User->guardian_email}}</td>
+                    </tr>
+                    <tr>
+                        <td>Guardian Email: </td>
+                        <td>{{$User->guardian_email}}</td>
                     </tr>
                 </tbody>
             </table>
