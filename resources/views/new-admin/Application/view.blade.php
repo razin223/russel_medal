8a072cf267727a17fd2ebe1c8d66368e99998992
razin223 <razin223@gmail.com>
2021-10-15 00:58:35 +0600

    Update view.blade.php

--- resources/views/new-admin/Application/view.blade.php
+++ resources/views/new-admin/Application/view.blade.php
@@ -15,6 +15,9 @@
             ?>
             <h4 class="text-center">{{$Sector->sector_name}}</h4>
             <table class="table table-bordered">
+                <tr>
+                    <td colspan="5" class="text-right">ID: {{$Data->id}}, Apply Date: {{date("d-m-Y h:i:sA",strtotime($Data->created_at))}}</td>
+                </tr>
                 <tbody>
                     <tr>
                         <td style="width: 150px">Name: </td>
